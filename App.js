f59932d4b3f0b257a50faea9f14478e2a1f62026
Felipe Rodrigues <felipeggrod@gmail.com>
2018-05-15 19:46:12 -0300

    Setup

---
+++ App.js
@@ -0,0 +1,20 @@
+
+
+// Create a function to wrap up your component
+function App(){
+return(
+<div>
+    <NavBar name="test string"/>
+</div>
+)
+}
+
+
+// Use the ReactDOM.render to show your component on the browser
+    ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+    );
+ 
+
+   
---
+++ Navbar.js
@@ -0,0 +1,75 @@
+// Create a ES6 class component    
+class ShoppingList extends React.Component { 
+    // Use the render function to return JSX component      
+    render() { 
+        return (
+        <div className="col">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+        </ul>
+        </div>
+        );
+    } 
+}
+
+const Card = (props) => {
+    return (
+        <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robots' src={"https://robohash.org/"+ props.id +"?200x200"} />
+            <div>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
+            </div>
+        </div>
+    );
+}
+
+// Create a ES6 class component    
+class NavBar extends React.Component { 
+    // Use the render function to return JSX component      
+    render() { 
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="#">Action</a>
+                            <a className="dropdown-item" href="#">Another action</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                        </ul>
+                        
+                    </div>
+                </nav>
+            </div>
+        );
+    } 
+}
+
+
+
+    
