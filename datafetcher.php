0d8cdc04cbf432f72cb0d1d5057835dacf6316b7
Felipe Rodrigues <felipergr@hotmail.com>
2018-05-17 16:08:19 -0300

    fixed json parsing on the server

--- datafetcher.php
+++ datafetcher.php
@@ -1,7 +1,11 @@
 <?php
+    
     //$jsonurl = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
+    header('Content-type: application/json');
     $jsonurl = "http://api.wipmania.com/json";
     $json = file_get_contents($jsonurl);
     //echo json_encode($json);
-    echo $_GET['jsonCallback'].$json;
+    //echo $_GET['jsonCallback'].$json;
+    echo $json;
+    
 ?>
