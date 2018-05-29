edc4a6cd3ca593a9ec1ba582af60fc2d7442c973
Felipe Rodrigues <felipergr@hotmail.com>
2018-05-29 18:22:04 -0300

    data crawling parameters

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # streamapp
 Web app that finds twitch streamers playing lesser known games
 
-The app is built with React
+The app is built with React and uses API services from Steam and Twitch.tv
--- datafetcherSteamPage.php
+++ datafetcherSteamPage.php
@@ -1,22 +1,31 @@
 <?php
     include 'simple_html_dom.php';
-    
-    header('Content-type: application/json');
 
+   // header('Content-type: application/json');
+
+    $start_page = $_POST['start_page']; // 1 is the 1st page
     $pages = $_POST['pages'];
     $tag = $_POST['tag'];
+    $sortby = $_POST['sortby']; // ASCD or DESC only
    
-
-    /*
-    echo '{"data":[';
-    foreach($html->find('span[class=title], div[class=search_released]') as $element) {
-       echo '{' . '"text:" "' . $element->plaintext . '"}';
+    
+    if ($sortby != 'DESC' || $sortby != 'ASCD' ) { //check for sortby
+        $sortby = 'DESC';
     }
-    echo ']}';
-    */
 
-    for ($x = 0; $x < 2; $x++) {
-        $html = file_get_html('https://store.steampowered.com/search/?sort_by=Released_ASCD');
+    if ($pages == '' || $pages == 0 ) { //undefined page amount
+        $pages = 1;
+    } else if ($pages > 100) {//upper limit for page amount
+        $pages = 100;
+    }
+
+    echo " start_page: " . $start_page;
+    echo "/ pages:" .$pages;
+    echo "/ tag:" .$tag;
+    echo "/ sortby:" .$sortby;
+
+    for ($x = 0; $x < $pages; $x++) {
+        $html = file_get_html('https://store.steampowered.com/search/?sort_by=Released_'.$sortby.'&page='. ($x + $start_page));
         foreach($html->find('a.search_result_row') as $article) {
             $item['appid'] = $article->find('img', 0)->src;
             $item['appid'] = substr($item['appid'], 43,-32);
@@ -24,10 +33,8 @@
             $item['date'] = $article->find('div.search_released', 0)->plaintext;
             $articles[] = $item;
         }
+        //echo $x; //simple debugger
     }
 
-    //print_r($articles); 
     print_r(json_encode($articles)); 
-    //echo $html->find('span[class=title]'); 
-    //echo $html;
 ?>
