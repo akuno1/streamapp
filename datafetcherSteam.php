f191ce2c448cce07e5b3f0ace177c8aa55308575
Felipe Rodrigues <felipergr@hotmail.com>
2018-06-04 15:38:22 -0300

    Remove temporary API requests and place holders

--- datafetcherSteam.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header('Content-type: application/json');
-
-    $jsonurl = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
-    $json = file_get_contents($jsonurl);
-    
-    echo $json;
-    
-?>
