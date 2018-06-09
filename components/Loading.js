5eb1f5545ce1805e046037ab71a1635d8ac62fa0
Felipe Rodrigues <felipergr@hotmail.com>
2018-06-08 22:00:31 -0300

    Searching as a waiting message makes more sense than Loading

--- components/Loading.js
+++ components/Loading.js
@@ -3,7 +3,7 @@ const Loading = (props) => {
     return (
         
             <div className="text-center pagination-centered centeringVertical">
-                <h1> Loading your stuff :) </h1>
+                <h1> Searching your stuff :) </h1>
                 <div className = "loader centering centeringVertical"> stuff </div> 
             </div>
        
