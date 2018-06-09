65f088ed2a80a97f2d7c11d4a2708d223a092222
Felipe Rodrigues <felipergr@hotmail.com>
2018-06-08 21:46:32 -0300

    Stream list view  formatting

--- components/StreamElement.js
+++ components/StreamElement.js
@@ -3,12 +3,12 @@ class StreamElement extends React.Component {
 
     render() {
         return (
-            <div className="col-4 col-sm-2 col-xl-1 " style={{padding: 0}}>
+            <div className="col-6 col-sm-4 col-md-3 col-xl-2 " style={{padding: 10}}>
                 <a href="#" onClick={(e) => this.props.chooseGame(this.props.twitchGameId, e)} style={{color: "#303030"}}>
                     <div className="card" style={{size: "100%"}}>
-                        <img className="card-img-top" style={{height: "5em"}} src={"https://steamcdn-a.akamaihd.net/steam/apps/"+ this.props.appid +"/header.jpg"} onError={(e)=>{e.target.src="media/placeholder.jpg"}}></img>
-                        <div className="card-body" style={{padding: 5}}>
-                            <p className="card-text" style={{fontSize: "1em", height: "3em"}}>{this.props.name}</p>
+                        <img className="card-img-top" style={{height: "10em"}} src={"https://steamcdn-a.akamaihd.net/steam/apps/"+ this.props.appid +"/header.jpg"} onError={(e)=>{e.target.src="media/placeholder.jpg"}}></img>
+                        <div className="card-body " style={{padding: 5}}>
+                            <p className="card-text text-center " style={{fontSize: "1em", height: "3em"}}>{this.props.name}</p>
                         </div>
                     </div>
                 </a>
