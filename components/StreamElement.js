ad3eb7bd4e15e4b65040207ee1eaf4f603265987
Felipe Rodrigues <felipergr@hotmail.com>
2018-06-20 12:42:34 -0300

    Grid columns to fit ipad screen

--- components/StreamElement.js
+++ components/StreamElement.js
@@ -3,7 +3,7 @@ class StreamElement extends React.Component {
 
     render() {
         return (
-            <div className="col-6 col-sm-4 col-md-3 col-xl-2 " style={{padding: 10}}>
+            <div className="col-6 col-sm-4 col-lg-3 col-xl-2 " style={{padding: 10}}>
                 <a href="#" onClick={(e) => this.props.chooseGame(this.props.twitchGameId, e)} style={{color: "#303030"}}>
                     <div className="card" style={{size: "100%"}}>
                         <img className="card-img-top" style={{height: "10em"}} src={"https://steamcdn-a.akamaihd.net/steam/apps/"+ this.props.appid +"/header.jpg"} onError={(e)=>{e.target.src="media/placeholder.jpg"}}></img>
