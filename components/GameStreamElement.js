a5bcc52cb2f789fe7d3c00eb44752a5cb82f021b
Felipe Rodrigues <felipergr@hotmail.com>
2018-06-20 12:50:29 -0300

    shadows on cards and grid sizing on gamestreamElements

--- components/GameStreamElement.js
+++ components/GameStreamElement.js
@@ -3,10 +3,10 @@ class GameStreamElement extends React.Component {
     render() {
         //console.log("GameStreamELEMENT");
         return (
-            <div className="col-4 col-sm-2 col-xl-1 " style={{padding: 0}}>
+            <div className=" col-6 col-sm-4 col-lg-3 col-xl-2" style={{padding: 10}}>
                 <a href="#" onClick={(e) => this.props.chooseChannel(this.props.channelId, e)} style={{color: "#303030"}}>
-                    <div className="card" style={{size: "100%"}}>
-                        <img className="card-img-top" style={{height: "5em"}} src={this.props.thumbnail_url.replace("{height}", "200").replace("{width}", "200")} onError={(e)=>{e.target.src="media/placeholder.jpg"}}></img>
+                    <div className="shadow card" style={{size: "100%"}}>
+                        <img className="card-img-top" style={{height: "10em"}} src={this.props.thumbnail_url.replace("{height}", "200").replace("{width}", "400")} onError={(e)=>{e.target.src="media/placeholder.jpg"}}></img>
                         <div className="card-body" style={{padding: 5}}>
                             <p className="card-text text-truncate "  style={{height: "3em" }}>
                                 {this.props.title} 
--- components/StreamElement.js
+++ components/StreamElement.js
@@ -5,7 +5,7 @@ class StreamElement extends React.Component {
         return (
             <div className="col-6 col-sm-4 col-lg-3 col-xl-2 " style={{padding: 10}}>
                 <a href="#" onClick={(e) => this.props.chooseGame(this.props.twitchGameId, e)} style={{color: "#303030"}}>
-                    <div className="card" style={{size: "100%"}}>
+                    <div className="shadow card" style={{size: "100%"}}>
                         <img className="card-img-top" style={{height: "10em"}} src={"https://steamcdn-a.akamaihd.net/steam/apps/"+ this.props.appid +"/header.jpg"} onError={(e)=>{e.target.src="media/placeholder.jpg"}}></img>
                         <div className="card-body " style={{padding: 5}}>
                             <p className="card-text text-center " style={{fontSize: "1em", height: "3em"}}>{this.props.name}</p>
